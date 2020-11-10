4852a183a443a257e9449bdce3232bc85f7fd63e
santosh-gslab <santosh.yadav@gslab.com>
2020-11-10 18:23:37 +0530

    Cleaning up the code

--- src/components/checkout-form/checkout-form.component.js
+++ src/components/checkout-form/checkout-form.component.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import { returnRegex } from '../../services';
 
-const regex = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
-
 class CheckoutForm extends Component {
     constructor(props) {
         super(props);
